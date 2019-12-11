bae5f3baa2060ba43bd5527411d1d67a432ff60c
CanyonFoot <CanyonFoot@gmail.com>
2019-12-11 12:08:41 -0800

    heckman code

--- code/Heckman2.R
+++ code/Heckman2.R
@@ -1,15 +1,15 @@
 ### Implementing Heckman correction - More Complex Case
 library(MASS)
 n <- 100000
-X <- matrix(c(rnorm(n, 0, 1), rnorm(n, 0, 1)), ncol = 2)
+X <- matrix(c(rnorm(n, 0, 1), rexp(n, 0, 1)), ncol = 2)
 Betas <- c(1.25, 3.125)
 
-W <- matrix(c(rnorm(n, 0, 1), rnorm(n, 0, 1)), ncol = 2)
+W <- matrix(c(rnorm(n, 0, 1), rexp(n, 0, 1)), ncol = 2)
 Deltas <- c(2.5, 1.5)
 
 ep_tau <- mvrnorm(n = n, mu  = c(0,0), Sigma = matrix(c(2, 1.5, 1.5, 2), byrow = T, nrow =2))
 
-Y <-  (X %*% Betas) + ep_tau[,1]
+Y <- (X %*% Betas) + ep_tau[,1]
 Z <- (W %*% Deltas) + ep_tau[,2]
 
 sample_vec <- ep_tau[,2] > - (W %*% Deltas)
@@ -34,14 +34,18 @@ m_adjusted <- lm(sampled_Y ~ sampled_X + bias_term[sample_vec])
 
 m_adjusted
  
-# Estimating the bias term
+### Adjusting for Bias
 
-sampling_probit <- glm(sample_vec ~ W, family = binomial(link = "probit"))
-estimated_deltas <- unname(sampling_probit$coefficients[2:3])
 
+# Estimate sigma_tau from squared residuals of model
 sigma_mod <- lm(Z ~ W)
 estimated_sigma_tau <- sqrt(sum((sigma_mod$residuals)^2) / (n - 2))
 
+# Estimate deltas as probit coefficents scaled by sigma_tau
+sampling_probit <- glm(sample_vec ~ W, family = binomial(link = "probit"))
+estimated_deltas <- unname(sampling_probit$coefficients[2:3]) * estimated_sigma_tau
+
+# Estimate bias term 
 estimated_V <- -(W %*% estimated_deltas) / estimated_sigma_tau
 estimated_bias <- (dnorm(estimated_V) / (estimated_sigma_tau * (1 - pnorm(estimated_V))))
 
